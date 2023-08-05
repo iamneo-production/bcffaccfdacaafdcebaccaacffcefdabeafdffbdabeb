d8cd37352a172959e7a0bbf4986e09767288f0b2
Iamneo <techadmin@iamneo.ai>
2023-08-05 14:21:12 +0000

    Backup Commit

--- reactapp/src/functions/filterOddEven.js
+++ reactapp/src/functions/filterOddEven.js
@@ -4,5 +4,13 @@ const filterOdd = (...numbers)=>
 };
 const filterEven = (...numbers)=>
 {
-    return numbers.filter(num=>num%2!)
-}
+    return numbers.filter(num=>num%2==0);
+};
+const generateRandomList = (n=7)=>
+{
+    return Array.from({length:n},()=>Math.floor(Math.random()*100+1));
+};
+
+export {
+    filterOdd, filterEven,generateRandomList
+};
